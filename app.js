85a109fc4faa89ae13363713e61c6e509980675f
Prashan5903s <chaubeyprashant498@gmail.com>
2025-04-28 17:50:59 +0530

    Database connection done

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+
+const MongoURL = process.env.Mongodb_url;
 
 const app = express();
 
-app.listen(3000, () => {
-    console.log("Server started!");
-});
+mongoose.connect(MongoURL)
+    .then(() => {
+        app.listen(3000, () => {
+            console.log("Server started on 3000!");
+        });
+    })
+    .catch(err => {
+        console.error('MongoDB connection error:', err);
+    });
