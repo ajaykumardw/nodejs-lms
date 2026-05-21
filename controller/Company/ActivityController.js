d36250fc2c29097a641db149f19e5b9c053dab51
Prashan5903s <chaubeyprashant498@gmail.com>
2026-05-21 13:11:50 +0530

    Change of scorm zip

--- controller/Company/ActivityController.js
+++ controller/Company/ActivityController.js
@@ -1,293 +1,379 @@
-const AppConfig = require('../../model/AppConfig');
+const path = require('path')
+const fs = require('fs')
+const unzipper = require('unzipper')
+
+const AppConfig = require('../../model/AppConfig')
 const Activity = require('../../model/Activity')
 const mongoose = require('mongoose')
-const { errorResponse, successResponse } = require('../../util/response');
-const Module = require('../../model/Module');
+const { errorResponse, successResponse } = require('../../util/response')
+const Module = require('../../model/Module')
 
 exports.getActivityAPI = async (req, res, next) => {
-    try {
-        const userId = req.userId;
-        const module_id = req.params.moduleId;
+  try {
+    const userId = req.userId
+    const module_id = req.params.moduleId
 
-        const activities = await Activity.aggregate([
-            {
-                $match: {
-                    created_by: mongoose.Types.ObjectId.createFromHexString(userId),
-                    module_id: mongoose.Types.ObjectId.createFromHexString(module_id)
-                }
-            },
-            {
-                $lookup: {
-                    from: 'app_config',
-                    let: { moduleTypeId: '$module_type_id' },
-                    pipeline: [
-                        { $unwind: '$activity_data' },
-                        { $match: { $expr: { $eq: ['$activity_data._id', '$$moduleTypeId'] } } },
-                        { $project: { _id: 0, activity_data: 1 } }
-                    ],
-                    as: 'activity_type',
-                }
-            },
-            {
-                $unwind: {
-                    path: '$activity_type',
-                    preserveNullAndEmptyArrays: true
-                }
-            },
-            // Populate virtual 'questions'
-            {
-                $lookup: {
-                    from: 'questions',            // collection name
-                    localField: '_id',            // Activity _id
-                    foreignField: 'activity_id',  // questions.activity_id
-                    as: 'questions'               // result array
-                }
-            }
-        ]);
-
-        if (!activities) {
-            return errorResponse(res, "Activity does not exist", {}, 404)
+    const activities = await Activity.aggregate([
+      {
+        $match: {
+          created_by: mongoose.Types.ObjectId.createFromHexString(userId),
+          module_id: mongoose.Types.ObjectId.createFromHexString(module_id)
         }
+      },
+      {
+        $lookup: {
+          from: 'app_config',
+          let: { moduleTypeId: '$module_type_id' },
+          pipeline: [
+            { $unwind: '$activity_data' },
+            {
+              $match: {
+                $expr: { $eq: ['$activity_data._id', '$$moduleTypeId'] }
+              }
+            },
+            { $project: { _id: 0, activity_data: 1 } }
+          ],
+          as: 'activity_type'
+        }
+      },
+      {
+        $unwind: {
+          path: '$activity_type',
+          preserveNullAndEmptyArrays: true
+        }
+      },
+      // Populate virtual 'questions'
+      {
+        $lookup: {
+          from: 'questions', // collection name
+          localField: '_id', // Activity _id
+          foreignField: 'activity_id', // questions.activity_id
+          as: 'questions' // result array
+        }
+      }
+    ])
 
-        return successResponse(res, "Activity fetched successfully", activities)
-
-    } catch (error) {
-        next(error)
+    if (!activities) {
+      return errorResponse(res, 'Activity does not exist', {}, 404)
     }
+
+    return successResponse(res, 'Activity fetched successfully', activities)
+  } catch (error) {
+    next(error)
+  }
 }
 
 exports.getCreateFormAPI = async (req, res, next) => {
-    try {
-        const appConfig = await AppConfig.findOne({ type: 'Activity_data' });
+  try {
+    const appConfig = await AppConfig.findOne({ type: 'Activity_data' })
 
-        if (!appConfig) {
-            return errorResponse(res, 'App config does not exist', {}, 404);
-        }
-
-        return successResponse(res, 'Create data fetched successfully', {
-            appConfig,
-        });
-    } catch (error) {
-        console.error('getCreateFormAPI error:', error);
-        return errorResponse(res, 'Internal Server Error', {}, 500);
+    if (!appConfig) {
+      return errorResponse(res, 'App config does not exist', {}, 404)
     }
-};
+
+    return successResponse(res, 'Create data fetched successfully', {
+      appConfig
+    })
+  } catch (error) {
+    console.error('getCreateFormAPI error:', error)
+    return errorResponse(res, 'Internal Server Error', {}, 500)
+  }
+}
 
 exports.postActivityFormAPI = async (req, res, next) => {
-    try {
-        const userId = req.userId;
-        const mId = req.params.moduleId;
-        const typeId = req.params.typeId;
+  try {
+    const userId = req.userId
+    const mId = req.params.moduleId
+    const typeId = req.params.typeId
 
-        const activity = new Activity({
-            created_by: userId,
-            module_id: mId,
-            module_type_id: typeId
-        })
+    const activity = new Activity({
+      created_by: userId,
+      module_id: mId,
+      module_type_id: typeId
+    })
 
-        await activity.save()
+    await activity.save()
 
-        await Module.findByIdAndUpdate(mId, {
-            is_survey_completed: false,
-            is_survey_done: false
-        })
+    await Module.findByIdAndUpdate(mId, {
+      is_survey_completed: false,
+      is_survey_done: false
+    })
 
-        return successResponse(res, "Activity saved successfully")
-
-    } catch (error) {
-        next(error)
-    }
+    return successResponse(res, 'Activity saved successfully')
+  } catch (error) {
+    next(error)
+  }
 }
 
 exports.deleteActivityAPI = async (req, res, next) => {
-    try {
+  try {
+    const userId = req.userId
+    const moduleId = req.params.moduleId
+    const id = req.params.id
 
-        const userId = req.userId;
-        const moduleId = req.params.moduleId;
-        const id = req.params.id;
+    const activity = await Activity.findOne({
+      created_by: userId,
+      module_id: moduleId,
+      _id: id
+    })
 
-        const activity = await Activity.findOne({ created_by: userId, module_id: moduleId, _id: id })
-
-        if (!activity) {
-            return errorResponse(res, "Activity does not exist", {}, 404)
-        }
-
-        await Activity.findOneAndDelete({ created_by: userId, module_id: moduleId, _id: id })
-
-        return successResponse(res, "Activity deleted successfully")
-
-    } catch (error) {
-        next(error)
+    if (!activity) {
+      return errorResponse(res, 'Activity does not exist', {}, 404)
     }
+
+    await Activity.findOneAndDelete({
+      created_by: userId,
+      module_id: moduleId,
+      _id: id
+    })
+
+    return successResponse(res, 'Activity deleted successfully')
+  } catch (error) {
+    next(error)
+  }
 }
 
 exports.setNameActivityAPI = async (req, res, next) => {
-    try {
+  try {
+    const userId = req.userId
+    const moduleId = req.params.moduleId
+    const id = req.params.id
 
-        const userId = req.userId;
-        const moduleId = req.params.moduleId;
-        const id = req.params.id;
+    const { title } = req.body
 
-        const { title } = req.body;
+    const activity = await Activity.findOne({
+      created_by: userId,
+      module_id: moduleId,
+      _id: id
+    })
 
-        const activity = await Activity.findOne({ created_by: userId, module_id: moduleId, _id: id })
-
-        if (!activity) {
-            return errorResponse(res, "Activity does not exist", {}, 404)
-        }
-
-        await Activity.findOneAndUpdate({ created_by: userId, module_id: moduleId, _id: id }, {
-            $set: {
-                name: title
-            }
-        })
-
-        return successResponse(res, "Activity saved successfully")
-
-    } catch (error) {
-        next(error);
+    if (!activity) {
+      return errorResponse(res, 'Activity does not exist', {}, 404)
     }
+
+    await Activity.findOneAndUpdate(
+      { created_by: userId, module_id: moduleId, _id: id },
+      {
+        $set: {
+          name: title
+        }
+      }
+    )
+
+    return successResponse(res, 'Activity saved successfully')
+  } catch (error) {
+    next(error)
+  }
 }
 
 exports.postActivityDataAPI = async (req, res, next) => {
-    try {
-        const { moduleId, moduleTypeId, id } = req.params;
-        const userId = req.userId;
+  try {
+    const { moduleId, moduleTypeId, id } = req.params
+    const userId = req.userId
 
-        const activity = await Activity.findOne({
-            created_by: userId,
-            module_id: moduleId,
-            module_type_id: moduleTypeId,
-            _id: id
-        });
+    const activity = await Activity.findOne({
+      created_by: userId,
+      module_id: moduleId,
+      module_type_id: moduleTypeId,
+      _id: id
+    })
 
-        if (!activity)
-            return errorResponse(res, "Activity does not exist", {}, 404);
+    if (!activity) {
+      return errorResponse(res, 'Activity does not exist', {}, 404)
+    }
 
-        const { title, video_url } = req.body;
-        const file = req.file;
+    const { title, video_url } = req.body
+    const file = req.file
 
-        const updatePayload = {};
+    const updatePayload = {}
 
-        if (moduleTypeId === "688723af5dd97f4ccae68834") {
+    // ---------------------------------------------------
+    // DOCUMENT
+    // ---------------------------------------------------
 
-            if (!activity?.document_data?.image_url && !file?.filename) {
+    if (moduleTypeId === '688723af5dd97f4ccae68834') {
+      if (!activity?.document_data?.image_url && !file?.filename) {
+        return errorResponse(res, 'File does not exist', {}, 400)
+      }
 
-                return errorResponse(res, "File does not exist", {}, 400);
-            }
+      updatePayload.document_data = {
+        title,
+        image_url: file?.filename || activity.document_data?.image_url || ''
+      }
+    }
 
-            updatePayload.document_data = {
-                title,
-                image_url: file?.filename || activity.document_data?.image_url || ""
-            };
-        } else if (moduleTypeId === "688723af5dd97f4ccae68835") {
+    // ---------------------------------------------------
+    // VIDEO FILE
+    // ---------------------------------------------------
+    else if (moduleTypeId === '688723af5dd97f4ccae68835') {
+      if (!activity?.video_data?.video_url && !file?.filename) {
+        return errorResponse(res, 'File does not exist', {}, 404)
+      }
 
-            if (!activity?.video_data?.video_url && !file?.filename) {
+      updatePayload.video_data = {
+        title,
+        video_url: file?.filename || activity.video_data?.video_url || ''
+      }
+    }
 
-                return errorResponse(res, "File does not exist", {}, 404);
-            }
+    // ---------------------------------------------------
+    // VIDEO URL
+    // ---------------------------------------------------
+    else if (moduleTypeId === '688723af5dd97f4ccae68836') {
+      if (!activity?.video_data?.video_url && !video_url) {
+        return errorResponse(res, 'File does not exist', {}, 404)
+      }
 
-            updatePayload.video_data = {
-                title,
-                video_url: file?.filename || activity.video_data?.video_url || ""
-            };
-        } else if (moduleTypeId === "688723af5dd97f4ccae68836") {
+      updatePayload.video_data = {
+        title,
+        video_url: video_url || activity.video_data?.video_url || ''
+      }
+    }
 
-            if (!activity?.video_data?.video_url && !video_url) {
+    // ---------------------------------------------------
+    // SCORM
+    // ---------------------------------------------------
+    else if (moduleTypeId === '688723af5dd97f4ccae68837') {
+      if (!file?.filename && !activity?.scorm_data?.folder_url) {
+        return errorResponse(res, 'SCORM ZIP required', {}, 400)
+      }
 
-                return errorResponse(res, "File does not exist", {}, 404);
-            }
+      if (file?.filename) {
+        const folderName = path.parse(file.filename).name
 
-            updatePayload.video_data = {
-                title,
-                video_url: video_url || activity.video_data?.video_url || ""
-            };
-        } else if (moduleTypeId === "688723af5dd97f4ccae68837") {
+        const zipFilePath = path.resolve(
+          process.cwd(),
+          'public',
+          'activity',
+          file.filename
+        )
 
+        const extractPath = path.resolve(
+          process.cwd(),
+          'public',
+          'activity',
+          folderName
+        )
 
-            const folderPath = req?.scormExtractedPath;        // activity/<folderName>
-            const folderName = folderPath?.split('/')?.pop();   // only folderName
-
-            if (!activity && !req.scormExtractedPath) {
-
-                return errorResponse(res, "Invalid SCORM ZIP file", {}, 400);
-            }
-
-            updatePayload.scorm_data = {
-                title,
-                folder_url: folderPath || activity?.scorm_data?.folder_url,                        // path relative to public
-                folder_name: folderName || activity?.scorm_data?.folder_name,
-                launch_file: req.scormLaunchFile || null       // save launch HTML file
-            };
-        } else {
-            return errorResponse(res, "Unsupported moduleTypeId", {}, 400);
+        updatePayload.scorm_data = {
+          title,
+          folder_url: `activity/${folderName}`,
+          folder_name: folderName,
+          zip_file: file.filename,
+          scorm_status: 'processing'
         }
 
-        await Activity.findByIdAndUpdate(
-            id,
-            { $set: updatePayload },
-            { new: true }
-        );
+        // ---------------------------------------------------
+        // BACKGROUND EXTRACTION
+        // ---------------------------------------------------
 
-        return successResponse(res, "Activity data uploaded successfully");
+        setImmediate(async () => {
+          try {
+            if (!fs.existsSync(extractPath)) {
+              fs.mkdirSync(extractPath, { recursive: true })
+            }
 
-    } catch (error) {
-        next(error);
+            await new Promise((resolve, reject) => {
+              fs.createReadStream(zipFilePath)
+                .pipe(unzipper.Extract({ path: extractPath }))
+                .on('close', resolve)
+                .on('error', reject)
+            })
+
+            // delete zip after extraction
+            fs.unlinkSync(zipFilePath)
+
+            // update status
+            await Activity.findByIdAndUpdate(id, {
+              $set: {
+                'scorm_data.scorm_status': 'completed'
+              }
+            })
+
+            console.log('SCORM extracted:', folderName)
+          } catch (err) {
+            console.error('SCORM extraction failed:', err)
+
+            await Activity.findByIdAndUpdate(id, {
+              $set: {
+                'scorm_data.scorm_status': 'failed'
+              }
+            })
+          }
+        })
+      }
     }
-};
+
+    // ---------------------------------------------------
+    // INVALID MODULE
+    // ---------------------------------------------------
+    else {
+      return errorResponse(res, 'Unsupported moduleTypeId', {}, 400)
+    }
+
+    // ---------------------------------------------------
+    // UPDATE DB
+    // ---------------------------------------------------
+
+    await Activity.findByIdAndUpdate(id, { $set: updatePayload }, { new: true })
+
+    return successResponse(res, 'Activity data uploaded successfully')
+  } catch (error) {
+    next(error)
+  }
+}
 
 exports.getLiveSessionController = async (req, res, next) => {
-    try {
+  try {
+    const userId = req?.userId
+    const { moduleId } = req?.params
 
-        const userId = req?.userId;
-        const { moduleId } = req?.params;
+    const module = await Module.findOne({
+      created_by: userId,
+      _id: moduleId
+    })
 
-        const module = await Module.findOne({
-            created_by: userId,
-            _id: moduleId
-        })
-
-        if (!module) {
-            return errorResponse(res, "Module not found", {}, 404)
-        }
-
-        return successResponse(res, "Module fetched successfully", module)
-
-    } catch (error) {
-        next(error)
+    if (!module) {
+      return errorResponse(res, 'Module not found', {}, 404)
     }
+
+    return successResponse(res, 'Module fetched successfully', module)
+  } catch (error) {
+    next(error)
+  }
 }
 
 exports.postLiveSessionController = async (req, res, next) => {
-    try {
+  try {
+    const userId = req?.userId
 
-        const userId = req?.userId;
+    const { moduleId } = req?.params
 
-        const { moduleId } = req?.params;
+    const { presenter, startDateTime, endDateTime } = req?.body
 
-        const { presenter, startDateTime, endDateTime } = req?.body;
+    const module = await Module.findOne({
+      created_by: userId,
+      _id: moduleId
+    })
 
-        const module = await Module.findOne({
-            created_by: userId,
-            _id: moduleId
-        })
-
-        if (!module) {
-            return errorResponse(res, "Module not found", {}, 404)
-        }
-
-        await Module.findOneAndUpdate({
-            created_by: userId,
-            _id: moduleId
-        }, {
-            presenter_id: presenter,
-            start_live_time: startDateTime,
-            end_live_time: endDateTime
-        })
-
-        return successResponse(res, "Live session saved successfully")
-
-    } catch (error) {
-        next(error)
+    if (!module) {
+      return errorResponse(res, 'Module not found', {}, 404)
     }
+
+    await Module.findOneAndUpdate(
+      {
+        created_by: userId,
+        _id: moduleId
+      },
+      {
+        presenter_id: presenter,
+        start_live_time: startDateTime,
+        end_live_time: endDateTime
+      }
+    )
+
+    return successResponse(res, 'Live session saved successfully')
+  } catch (error) {
+    next(error)
+  }
 }
--- util/upload.js
+++ util/upload.js
@@ -1,90 +1,94 @@
-const multer = require("multer");
-const path = require("path");
-const fs = require("fs");
-const pdfParse = require("pdf-parse");
-const unzipper = require("unzipper");
+const multer = require('multer')
+const path = require('path')
+const fs = require('fs')
 
-function createUpload(allowedTypes, directory = "uploads/", maxSizeMB = 2000) {
+function createUpload (allowedTypes, directory = 'uploads/', maxSizeMB = 2000) {
+  const uploadPath = `/public/${directory}`
 
-  const uploadPath = `/public/${directory}`;
-  const absPath = path.join(__dirname, "..", uploadPath);
+  const absPath = path.join(__dirname, '..', uploadPath)
 
+  // create directory
   if (!fs.existsSync(absPath)) {
-    fs.mkdirSync(absPath, { recursive: true });
+    fs.mkdirSync(absPath, { recursive: true })
   }
 
-  // Storage system
+  // ---------------------------------------------------
+  // STORAGE
+  // ---------------------------------------------------
+
   const storage = multer.diskStorage({
-    destination: (req, file, cb) => cb(null, absPath),
-    filename: (req, file, cb) => {
-      const ext = path.extname(file.originalname);
-      const name = `${Date.now()}-${Math.round(Math.random() * 9999)}${ext}`;
-      cb(null, name);
+    destination: (req, file, cb) => {
+      cb(null, absPath)
     },
-  });
+
+    filename: (req, file, cb) => {
+      const ext = path.extname(file.originalname)
+
+      const fileName = `${Date.now()}-${Math.round(Math.random() * 9999)}${ext}`
+
+      cb(null, fileName)
+    }
+  })
+
+  // ---------------------------------------------------
+  // MULTER
+  // ---------------------------------------------------
 
   const upload = multer({
     storage,
-    limits: { fileSize: maxSizeMB * 1024 * 1024 }, // 2GB support
-    fileFilter: (req, file, cb) => {
-      if (allowedTypes.includes(file.mimetype)) cb(null, true);
-      else cb(new Error("Invalid file type"), false);
+
+    limits: {
+      fileSize: maxSizeMB * 1024 * 1024
     },
-  });
 
-  const handleZipOrPdf = async (req, res, next) => {
-    try {
-      if (!req.file) return next();
-
-      const filePath = path.join(absPath, req.file.filename);
-
-      if (req.file.mimetype === "application/pdf") {
-        const buffer = fs.readFileSync(filePath);
-        const data = await pdfParse(buffer);
-        req.pdfPageCount = data.numpages;
+    fileFilter: (req, file, cb) => {
+      if (allowedTypes.includes(file.mimetype)) {
+        cb(null, true)
+      } else {
+        cb(new Error('Invalid file type'))
       }
-
-      if (
-        req.file.mimetype === "application/zip" ||
-        req.file.mimetype === "application/x-zip-compressed" ||
-        req.file.mimetype === "multipart/x-zip"
-      ) {
-        const folderName = req.file.filename.replace(".zip", "");
-        const extractPath = path.join(absPath, folderName);
-
-        if (!fs.existsSync(extractPath)) {
-          fs.mkdirSync(extractPath);
-        }
-
-        await new Promise((resolve, reject) => {
-          fs.createReadStream(filePath)
-            .pipe(unzipper.Extract({ path: extractPath }))
-            .on("close", resolve)
-            .on("error", reject);
-        });
-
-        fs.unlinkSync(filePath);
-
-        req.scormExtractedPath = `${directory}/${folderName}`;
-      }
-
-      next();
-    } catch (err) {
-      console.error("SCORM/PDF Processing Error:", err);
-      next(err);
     }
-  };
+  })
+
+  // ---------------------------------------------------
+  // MIDDLEWARE
+  // ---------------------------------------------------
 
   return {
-    middleware: (fieldName = "file") => [
+    middleware: (fieldName = 'file') => [
       (req, res, next) => {
-        req.uploadPath = directory;
-        next();
+        req.uploadPath = directory
+        next()
       },
-      upload.single(fieldName),
-      handleZipOrPdf,
-    ],
-  };
+
+      (req, res, next) => {
+        upload.single(fieldName)(req, res, err => {
+          if (err instanceof multer.MulterError) {
+            if (err.code === 'LIMIT_FILE_SIZE') {
+              return res.status(400).json({
+                status: 'Failure',
+                message: 'File too large'
+              })
+            }
+
+            return res.status(400).json({
+              status: 'Failure',
+              message: err.message
+            })
+          }
+
+          if (err) {
+            return res.status(400).json({
+              status: 'Failure',
+              message: err.message
+            })
+          }
+
+          next()
+        })
+      }
+    ]
+  }
 }
 
-module.exports = createUpload;
+module.exports = createUpload
