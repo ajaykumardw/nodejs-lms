c64c3e96bec59f0f35dc2402f09e526d4214c386
Prashan5903s <chaubeyprashant498@gmail.com>
2026-07-03 16:49:26 +0530

    Changes of percentage in dashboard

--- controller/User/DashboardAPIController.js
+++ controller/User/DashboardAPIController.js
@@ -4,6 +4,7 @@ const Activity = require('../../model/Activity')
 const AppConfig = require('../../model/AppConfig')
 const NotificationLog = require('../../model/NotificationLog')
 const ActivityLog = require('../../model/ActivityFolderReport')
+const SettingConfig = require('../../model/settingConfig')
 const { successResponse } = require('../../util/response')
 
 const dayjs = require('dayjs')
@@ -17,6 +18,25 @@ exports.getDashboardAPI = async (req, res, next) => {
     const liveSessionId = mongoose.Types.ObjectId.createFromHexString(
       '688219557b6953e899cb57d3'
     )
+
+    const user = await User.findById(userObjectId)
+
+    if (!user) {
+      return errorResponse(res, 'User does not exist', {}, 404)
+    }
+
+    const masterId = user?.created_by
+
+    const now = new Date()
+
+    const settingConfig = await SettingConfig.findOne({
+      type: 'certificate_setting',
+      created_by: masterId
+    })
+
+    const certificateSettingId = settingConfig?.certificate_setting_data_id
+      ? settingConfig.certificate_setting_data_id
+      : '6a153d4a393b1c736064377b'
 
     const module = await Module.aggregate([
       {
@@ -190,19 +210,410 @@ exports.getDashboardAPI = async (req, res, next) => {
 
     const moduleIds = module.map(m => m._id)
 
-    const modules = await Module.find({
-      _id: { $in: moduleIds }
-    })
-      .populate({
-        path: 'activities',
-        populate: {
-          path: 'logs',
-          match: {
-            user_id: userObjectId
+    const modules = await Module.aggregate([
+      {
+        $match: {
+          created_by: masterId
+        }
+      },
+      {
+        $lookup: {
+          from: 'activity',
+          localField: '_id',
+          foreignField: 'module_id',
+          as: 'activity'
+        }
+      },
+
+      // GET USER ACTIVITY LOGS
+      {
+        $lookup: {
+          from: 'activity_logs',
+          let: { user_id: userId, module_id: '$_id' },
+          pipeline: [
+            {
+              $match: {
+                $expr: {
+                  $and: [
+                    { $eq: ['$user_id', '$$user_id'] },
+                    { $eq: ['$module_id', '$$module_id'] }
+                  ]
+                }
+              }
+            }
+          ],
+          as: 'activity_logs'
+        }
+      },
+
+      // PROGRAM SCHEDULE
+      {
+        $lookup: {
+          from: 'program_schedules',
+          localField: '_id',
+          foreignField: 'module_id',
+          as: 'programSchedule'
+        }
+      },
+
+      {
+        $unwind: {
+          path: '$programSchedule',
+          preserveNullAndEmptyArrays: true
+        }
+      },
+
+      // MODULE ENROLL
+      {
+        $lookup: {
+          from: 'user_module_enroll',
+          let: { moduleId: '$_id', userId: userId },
+          pipeline: [
+            {
+              $match: {
+                $expr: {
+                  $and: [
+                    { $eq: ['$module_id', '$$moduleId'] },
+                    { $eq: ['$user_id', '$$userId'] }
+                  ]
+                }
+              }
+            }
+          ],
+          as: 'moduleEnroll'
+        }
+      },
+
+      {
+        $lookup: {
+          from: 'modulesettings',
+          localField: '_id',
+          foreignField: 'moduleId',
+          as: 'module_setting'
+        }
+      },
+
+      {
+        $unwind: {
+          path: '$module_setting',
+          preserveNullAndEmptyArrays: true
+        }
+      },
+
+      {
+        $addFields: {
+          is_certificate_enable: {
+            $eq: ['$module_setting.certificateEnabled', true]
           }
         }
-      })
-      .lean()
+      },
+
+      {
+        $lookup: {
+          from: 'certificates',
+          let: { certificateId: '$module_setting.selectedCertificateId' },
+          pipeline: [
+            {
+              $match: {
+                $expr: {
+                  $and: [{ $eq: ['$_id', '$$certificateId'] }]
+                }
+              }
+            }
+          ],
+          as: 'certificate'
+        }
+      },
+
+      {
+        $unwind: {
+          path: '$certificate',
+          preserveNullAndEmptyArrays: true
+        }
+      },
+
+      // RELATIVE DATE
+      {
+        $addFields: {
+          relativeEndDate: {
+            $cond: [
+              { $eq: ['$programSchedule.dueType', 'relative'] },
+              {
+                $dateAdd: {
+                  startDate: '$programSchedule.published_date',
+                  unit: 'day',
+                  amount: {
+                    $toInt: {
+                      $ifNull: ['$programSchedule.dueDays', 0]
+                    }
+                  }
+                }
+              },
+              null
+            ]
+          }
+        }
+      },
+
+      // DATE VISIBILITY
+      {
+        $addFields: {
+          isDateVisible: {
+            $cond: [
+              { $eq: ['$programSchedule.dueType', 'relative'] },
+              {
+                $and: [
+                  { $lte: ['$programSchedule.published_date', now] },
+                  { $gte: ['$relativeEndDate', now] }
+                ]
+              },
+              {
+                $cond: [
+                  { $eq: ['$programSchedule.dueType', 'fixed'] },
+                  {
+                    $and: [
+                      { $lte: ['$programSchedule.dueDate.start_date', now] },
+                      { $gte: ['$programSchedule.dueDate.end_date', now] }
+                    ]
+                  },
+                  true
+                ]
+              }
+            ]
+          }
+        }
+      },
+
+      // ENROLLMENT VISIBILITY
+      {
+        $addFields: {
+          isVisible: {
+            $cond: {
+              if: { $eq: ['$programSchedule.pushEnrollmentSetting', 2] },
+              then: { $gt: [{ $size: '$moduleEnroll' }, 0] },
+              else: true
+            }
+          }
+        }
+      },
+
+      // LIVE MODULE VISIBILITY
+      {
+        $addFields: {
+          isLiveModuleVisible: {
+            $cond: [
+              { $eq: ['$module_type_id', LIVE_MODULE_TYPE_ID] },
+              {
+                $and: [
+                  { $lte: ['$start_live_time', now] },
+                  { $gte: ['$end_live_time', now] },
+                  { $gt: [{ $size: '$moduleEnroll' }, 0] }
+                ]
+              },
+              true
+            ]
+          }
+        }
+      },
+
+      // COMPLETED ACTIVITY COUNT
+      {
+        $addFields: {
+          total_activity: {
+            $size: '$activity'
+          },
+
+          completed_activity: {
+            $size: {
+              $filter: {
+                input: '$activity',
+                as: 'act',
+                cond: {
+                  $gt: [
+                    {
+                      $size: {
+                        $filter: {
+                          input: '$activity_logs',
+                          as: 'log',
+                          cond: {
+                            $and: [
+                              {
+                                $eq: ['$$log.activity_id', '$$act._id']
+                              },
+                              {
+                                $eq: ['$$log.is_completed', true]
+                              }
+                            ]
+                          }
+                        }
+                      }
+                    },
+                    0
+                  ]
+                }
+              }
+            }
+          }
+        }
+      },
+
+      // COMPLETION PERCENTAGE
+      {
+        $addFields: {
+          completion_percentage: {
+            $cond: [
+              { $eq: ['$total_activity', 0] },
+              0,
+              {
+                $round: [
+                  {
+                    $multiply: [
+                      {
+                        $divide: ['$completed_activity', '$total_activity']
+                      },
+                      100
+                    ]
+                  },
+                  0
+                ]
+              }
+            ]
+          }
+        }
+      },
+
+      // HAS COMPLETED
+      // HAS COMPLETED
+      {
+        $addFields: {
+          has_completed: {
+            $cond: [
+              {
+                $ne: [
+                  certificateSettingId,
+                  mongoose.Types.ObjectId.createFromHexString(
+                    '6a153d4a393b1c736064377b'
+                  )
+                ]
+              },
+              false,
+              {
+                $cond: [
+                  {
+                    $ne: [
+                      '$module_type_id',
+                      mongoose.Types.ObjectId.createFromHexString(
+                        '688219557b6953e899cb57d2'
+                      )
+                    ]
+                  },
+                  false,
+                  {
+                    $and: [
+                      { $gt: ['$completed_activity', 0] },
+                      {
+                        $eq: ['$completed_activity', '$total_activity']
+                      }
+                    ]
+                  }
+                ]
+              }
+            ]
+          }
+        }
+      },
+
+      {
+        $addFields: {
+          module_completed_at: {
+            $cond: [
+              {
+                $eq: ['$completed_activity', '$total_activity']
+              },
+              {
+                $let: {
+                  vars: {
+                    completedLogs: {
+                      $filter: {
+                        input: '$activity_logs',
+                        as: 'log',
+                        cond: {
+                          $eq: ['$$log.is_completed', true]
+                        }
+                      }
+                    }
+                  },
+                  in: {
+                    $max: '$$completedLogs.completed_at_time'
+                  }
+                }
+              },
+              null
+            ]
+          }
+        }
+      },
+
+      {
+        $lookup: {
+          from: 'content_folder',
+          localField: 'content_folder_id',
+          foreignField: '_id',
+          as: 'contentFolder'
+        }
+      },
+
+      {
+        $unwind: {
+          path: '$contentFolder',
+          preserveNullAndEmptyArrays: true
+        }
+      },
+
+      {
+        $lookup: {
+          from: 'programs',
+          localField: 'contentFolder.program_id',
+          foreignField: '_id',
+          as: 'program'
+        }
+      },
+      {
+        $unwind: {
+          path: '$program',
+          preserveNullAndEmptyArrays: true
+        }
+      },
+
+      // FINAL FILTER
+      {
+        $match: {
+          isLiveModuleVisible: true,
+          isVisible: true,
+          'programSchedule._id': { $exists: true },
+          isDateVisible: true,
+          ...completionFilter
+        }
+      },
+
+      {
+        $project: {
+          title: 1,
+          image_url: 1,
+          completion_percentage: 1,
+          has_completed: 1,
+          module_completed_at: 1,
+          total_activity: 1,
+          completed_activity: 1,
+          description: 1,
+          contentFolderName: '$contentFolder.title',
+          programName: '$program.title',
+          contentFolderId: '$contentFolder._id',
+          programId: '$program._id',
+          activity: 1
+        }
+      }
+    ])
 
     let total = modules.length
     let completed = 0
@@ -213,7 +624,7 @@ exports.getDashboardAPI = async (req, res, next) => {
       const activities = module.activities || []
 
       // No activities => not started
-      if (!activities.length) {
+      if (!activity.length) {
         not_started++
         continue
       }
@@ -221,23 +632,9 @@ exports.getDashboardAPI = async (req, res, next) => {
       let completedActivities = 0
       let hasInProgress = false
 
-      for (const activity of activities) {
-        const logs = activity.logs || []
-
-        const hasCompleted = logs.some(log => log.progress_status === '3')
-
-        const hasProgress = logs.some(log => log.progress_status === '2')
-
-        if (hasCompleted) {
-          completedActivities++
-        } else if (hasProgress) {
-          hasInProgress = true
-        }
-      }
-
-      if (completedActivities === activities.length) {
+      if (total_activity === completed_activity) {
         completed++
-      } else if (hasInProgress) {
+      } else if (module?.completion_percentage > 0) {
         in_progress++
       } else {
         not_started++
